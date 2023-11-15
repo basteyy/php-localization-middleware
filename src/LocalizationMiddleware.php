450b0fe477d621f743d5befb7d5c38ce1e0026f9
Sebastian <sebastian@eiweleit.de>
2023-11-15 17:12:40 +0100

    Update LocalizationMiddleware.php

--- src/LocalizationMiddleware.php
+++ src/LocalizationMiddleware.php
@@ -29,18 +29,23 @@ class LocalizationMiddleware implements MiddlewareInterface {
     /** @var bool $patch_requested_url Patch the requested url to remove the language */
     private bool $patch_requested_url;
 
+    /** @var bool $patch_only_exactly_match Patch requested url only if the language is exactly matched */
+    private bool $patch_only_exactly_match;
+
     /** @var bool $browser_overwrite_url Overwrite the url with the browser language */
     private bool $browser_overwrite_url;
 
     public function __construct(?string $default_language = null,
                                 ?array $available_languages = null,
                                 ?bool $patch_requested_url = true,
-                                ?bool $browser_overwrite_url = false)
+                                ?bool $browser_overwrite_url = false,
+                                ?bool $patch_only_exactly_match = false)
     {
         $this->default_language = $default_language ?? null;
         $this->available_languages = $available_languages ?? [];
         $this->patch_requested_url = $patch_requested_url ?? true;
         $this->browser_overwrite_url = $browser_overwrite_url ?? false;
+        $this->patch_only_exactly_match = $patch_only_exactly_match ?? false;
     }
 
     /**
@@ -71,25 +76,15 @@ class LocalizationMiddleware implements MiddlewareInterface {
     /**
      * Get the preferred language from the browser (based on Accept-Language header)
      * @param RequestInterface $request
-     * @return string|null
+     * @return array
      */
-    private function getPreferredLanguage(RequestInterface $request): string|null
+    private function getPreferredLanguage(RequestInterface $request): array
     {
         $acceptLanguageHeader = $request->getHeader('Accept-Language');
 
-        $acceptedLanguages = explode(',', $acceptLanguageHeader[0]);
-
-        $preferredLanguages = array_map(function ($lang) {
+        return array_map(function ($lang) {
             return substr($lang, 0, 2);
-        }, $acceptedLanguages);
-
-        foreach ($preferredLanguages as $prefLang) {
-            if (in_array($prefLang, $this->available_languages)) {
-                return $prefLang;
-            }
-        }
-
-        return null;
+        }, explode(',', $acceptLanguageHeader[0]));
     }
 
     /**
@@ -104,7 +99,7 @@ class LocalizationMiddleware implements MiddlewareInterface {
             $lang = substr($url, 1, 2);
 
             if (in_array($lang, $this->available_languages)) {
-                return substr($url, 3);
+                return substr($url, 1, 2);
             }
         }
 
@@ -119,13 +114,26 @@ class LocalizationMiddleware implements MiddlewareInterface {
     {
         $browser = $this->getPreferredLanguage($request);
         $url = $this->getUrlLanguage($request);
-        if (!$browser && !$url) {
-            return  $this->default_language;
-        } elseif ($browser === $url && in_array($browser, $this->available_languages)) {
-            return $browser;
-        } elseif ($browser !== $url && $this->browser_overwrite_url && in_array($browser, $this->available_languages)) {
-            return $browser;
-        } elseif ($url !== null && in_array($url, $this->available_languages)) {
+
+        if (!$url) {
+            foreach ($browser as $lang) {
+                if (in_array($lang, $this->available_languages)) {
+                    return $lang;
+                }
+            }
+
+            return $this->default_language;
+        }
+
+        if ($this->browser_overwrite_url) {
+            foreach ($browser as $lang) {
+                if (in_array($lang, $this->available_languages)) {
+                    return $lang;
+                }
+            }
+        }
+
+        if (in_array($url, $this->available_languages)) {
             return $url;
         }
 
@@ -141,7 +149,7 @@ class LocalizationMiddleware implements MiddlewareInterface {
         $lang = $this->determineLanguage($request);
         $request = $request->withAttribute($this->attribute_name, $lang);
 
-        if ($this->patch_requested_url && str_starts_with($request->getUri()->getPath(), '/' . $lang . '/')) {
+        if ($this->patch_requested_url && (!$this->patch_only_exactly_match || str_starts_with($request->getUri()->getPath(), '/' . $lang . '/'))) {
             // new url for thr request
             $url = substr($request->getUri()->getPath(), 3);
             $uri = $request->getUri()->withPath($url);
